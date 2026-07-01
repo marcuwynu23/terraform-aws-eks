33b02f86b53ac1af83de81b05d73ce78f5b3fc60
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-02 00:21:18 +0800

    feat: add initial Terraform configuration for AWS and Kubernetes providers

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
