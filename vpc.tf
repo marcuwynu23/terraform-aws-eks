5e7a650b625ff612cd3b071bb99e886363a4ebeb
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-02 00:22:08 +0800

    feat: parameterize VPC and subnet configuration using variables

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "eks_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   tags                 = { Name = "eks-vpc" }
 }
@@ -8,7 +8,7 @@ resource "aws_subnet" "eks_subnet" {
   count                   = 2
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-southeast-1a", "ap-southeast-1b"], count.index)
+  availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
   tags                    = { Name = "eks-subnet-${count.index}" }
 }
